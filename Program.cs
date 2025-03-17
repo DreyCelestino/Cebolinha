f2f11f4eb082d09e8d324a13e1464de35728852c
Andrey Celestino <lab53@fatec.com>
2025-03-17 10:30:11 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+String frase;
+
+Console.WriteLine("Digite algo pro Cebolinha repetir!: ");
+frase = Console.ReadLine().Replace("r", "l");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"Cebolinha: {frase}");
+
+Console.ResetColor();
